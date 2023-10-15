79165573e3fbb846948817ed643a03f6e89a250d
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-15 23:20:58 +0300

    API

---
+++ CodePulse.API/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.Dtos;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryController(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto createCategoryRequestDto)
+        {
+            var category = _mapper.Map<Category>(createCategoryRequestDto);
+            await _categoryRepository.CreateAsync(category);
+
+            return Ok(category);
+        }
+
+    }
+}
---
+++ CodePulse.API/Mapping/MapProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.Dtos;
+
+namespace CodePulse.API.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Category, CreateCategoryRequestDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
