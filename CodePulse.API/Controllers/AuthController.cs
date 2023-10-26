6762d843226e9274496873bd8e49df6b41f5f934
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-10-27 02:05:28 +0300

    fix

--- CodePulse.API/Controllers/AuthController.cs
+++ CodePulse.API/Controllers/AuthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CodePulse.API.Data
+namespace CodePulse.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
